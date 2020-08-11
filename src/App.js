ea5f2ebff9418c395196a81481ddeb43b4ca573e
mromero <mgr_89@hotmail.com>
2020-08-11 17:42:32 -0300

    imagenes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes'
+
 
 
 function App() {
+
+  //state de la app
+  const[busqueda, guardarBusqueda]= useState('');
+  const [imagenes, guardarImagenes]= useState([]);
+  
+  useEffect(()=>{
+    const consultarAPI = async ()=>{
+      
+      if(busqueda==='')return;
+      const imagenesPorPagina=30;
+      const key='17858395-975b9ee0c60250745c1009edd';
+      const url=`https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+  
+      const respuesta = await fetch(url);
+      const resultado= await respuesta.json();
+      guardarImagenes(resultado.hits)
+    }
+    consultarAPI();
+
+
+  },[busqueda]);
+
+
   return (
     <div className='container'>
       <div className='jumbotron'>
         <p className='lead text-center'>Buscador de Imágenes</p>
 
-      <Formulario/>
+      <Formulario
+      guardarBusqueda={guardarBusqueda}
+      />
+      </div>
+      <div className='row justify-content-center'>
+      <ListadoImagenes
+      imagenes={imagenes}
+      />
+
       </div>
 
     </div>
